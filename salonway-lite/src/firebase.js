db6e8905c1de1223afd37779e3dde92044daef4a
SibaCode <mvubusiba@gmail.com>
2026-02-05 19:36:07 +0200

    tesr6

--- salonway-lite/src/firebase.js
+++ salonway-lite/src/firebase.js
@@ -14,7 +14,6 @@ const firebaseConfig = {
   measurementId: "G-M8YF7YCXD4"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
